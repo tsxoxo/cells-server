e54805239414edc77556f4755c79634a7ddd88e6
Tom Szwaja <tsxoxo@proton.me>
2025-07-26 22:25:41 +0200

    add sqlite

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const PORT = 3000;
 
+const db = require("better-sqlite3")("cells.db", { verbose: console.log });
+
+db.prepare(
+    `CREATE TABLE IF NOT EXISTS cells (
+  position INTEGER PRIMARY KEY,
+  content TEXT NOT NULL,
+  value TEXT,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);`,
+).run();
+const create_row = db.prepare(
+    " INSERT INTO cells (position, content, value) VALUES (?, ?, ?) ",
+);
+const cell = db.prepare(" SELECT * FROM cells WHERE position = ? ");
+
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 
 app.get("/", (req, res) => {
+    // TODO: Get the whole cells table
+    // Probably process it to add a layer of indirection
+    // and send Cell[] back to FE
     res.send("hey world");
 });
 
@@ -22,6 +40,12 @@ app.post("/cells", (req, res) => {
     res.set({
         "Access-Control-Allow-Origin": "*",
     });
+
+    // START_HERE: forEach(cellInPayload => cell.get(cellInPayload.ownIndex))
+    // if cellDB.content === cellPayload.oldContent then setToPayload, res(ok)
+    // res(err, cellDB.content)
+
+    // Add artificial delay to make UI reactions more pronounced
     setTimeout(
         () =>
             res.json({
@@ -34,3 +58,7 @@ app.post("/cells", (req, res) => {
 app.listen(PORT, () => {
     console.log("Express.js listening on port ", PORT);
 });
+
+const process = require("node:process");
+process.on("SIGINT", () => db.close());
+process.on("SIGTERM", () => db.close());
